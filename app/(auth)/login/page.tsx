5db25c39406d5c01ee6ebfc9e15ba732cba9c5a3
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-10-10 17:06:35 +0530

    Update page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -35,7 +35,7 @@ export default function LoginPage() {
     }
     const json = (await res.json()) as { accessToken: string };
     setAccessToken(json.accessToken);
-    router.push("/profile");
+    router.push("/");
   };
 
   return (

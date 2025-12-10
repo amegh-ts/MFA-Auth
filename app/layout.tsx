bdeefdab2015eea23c386ecbb180944354ebdafd
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-12-10 10:40:38 +0530

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { AccessTokenProvider } from "@/components/auth/access-token-provider";
-import Providers from "@/lib/ReactQueryClientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>
-          <AccessTokenProvider>{children}</AccessTokenProvider>
-        </Providers>
+        {children}
       </body>
     </html>
   );
